83373d3e6209315e86a36e0ecf3eb14e01b22932
manel <manelsancho1989@gmail.com>
2019-04-02 17:14:51 +0200

    tabulacio

--- resources/views/gestio/ticket/assign.blade.php
+++ resources/views/gestio/ticket/assign.blade.php
@@ -11,36 +11,47 @@
       margin-top: 40px;
     }
   </style>
-
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Tickets: Assignar  a Ticket</h1>
-          <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group mr-2">
-              <button class="btn btn-sm btn-outline-secondary" value="Exportar">
-                <span data-feather="save"></span>
-                Exportar
-              </button>
-            </div>
-          </div>
-        </div>
-        <form action="{{ route('ticket.crearAssignacioTicket', $ticket->id) }}">
+<div class="modal fade" id="ModalEmpleat{{$users->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Assignar Empleat</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="{{ route('atraccions.guardarAssignacio', $atraccio->id) }}" >
+      @csrf
+      <div class="modal-body">
         <div class="row">
-          <div class="col-4">
-            <label for="example-date-input" class="col-6 col-form-label">Data inici</label>
-            <input class="form-control" name="data_inici_assignacio_empleat" type="date">
+          <div class="col-6">
+            <span>Atraccio:</span>
+            <input type="text" class="form-control" name="id_atraccio" value="{{ $atraccio->id }}" hidden/>
+            <input type="text" class="form-control" name="nom_atraccio" value="{{ $atraccio->nom_atraccio }}" disabled />
           </div>
-          <div class="col-4">
-            <label for="example-date-input" class="col-6 col-form-label">Data fi</label>
-            <input class="form-control" name="data_fi_assignacio_empleat"  type="date">
+          <div class="col-6">
+            <span>Empleat:</span>
+            <input type="text" class="form-control" name="id_empleat" value="{{ $users->id }}" hidden />
+            <input type="text" class="form-control" name="nom_empleat" readonly value="{{ $users->nom}}"/>
           </div>
-          <div class="col-4">
-<br>
-<br>
-     <button type="submit" class="btn">Enviar</button>
+          <div class="col-6">
+            <span>Data Inici:</span>
+            <input type="date" class="form-control" name="data_inici_modal" readonly value="{{ $data_inici_global}}"/>
           </div>
-        </div>
-</form>
 
+          <div class="col-6">
+            <span>Data Fi:</span>
+            <input type="date" class="form-control" name="data_fi_modal" readonly value="{{ $data_fi_global}}"/>
           </div>
+          <br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+        <button type="submit" class="btn btn-primary">Finalitzar assignament</button>
+      </div>
+      </form>
+      </div>
+  </div>
+</div>
 
 @endsection
