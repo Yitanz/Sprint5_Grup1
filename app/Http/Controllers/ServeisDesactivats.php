13d49669fb8e44351a0ba942105a4c1d9a74a56f
manugallego <manuxp2010@gmail.com>
2019-04-04 14:34:56 +0200

    Create ServeisDesactivats.php

---
+++ app/Http/Controllers/ServeisDesactivats.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ServeisDesactivats extends Controller
+{
+  public function index()
+  {
+    $assignacions = Zona::where('id_estat',3)
+    ->join ('serveis_zones','serveis_zones.id_zona', '=', 'zones.id')
+    ->join('users', 'serveis_zones.id_empleat', '=', 'users.id')
+    ->join('serveis', 'serveis_zones.id_servei', '=', 'serveis.id')
+    ->get ([
+      'serveis_zones.id as id',
+      'zones.nom as nom_zona',
+      'serveis.nom as nom_servei',
+      'users.nom as nom_empleat'
+    ]);
+
+    return view('gestio/serveis/index', compact('assignacions'));
+  }
+}
